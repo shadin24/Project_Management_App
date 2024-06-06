6bdd22952f5194668d706171740d8de5851a4fe3
shadin24 <shadinkvr@gmail.com>
2024-06-06 16:46:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NewProject from "./components/NewProject";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar/>
+      <NewProject/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const classes='w-full p-1 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
+
+export default function Input({label,textarea,...props}) {
+  return (
+    <p className='flex flex-col gap-1 my-4'>
+        <label className='text-sm font-bold uppercase text-stone-500'>
+            {label}
+        </label>
+        {textarea ? <textarea className={classes} {...props}/> : <input  className={classes}{...props}/>}
+    </p>
+  )
+}
